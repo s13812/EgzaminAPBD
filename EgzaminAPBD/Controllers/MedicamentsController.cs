48dfa8fc00882e2de3a766b79e563778839be349
Michal Razowski <s13812@pjwstk.edu.pl>
2021-02-14 11:41:22 +0000

    work started

---
+++ EgzaminAPBD/Controllers/MedicamentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EgzaminAPBD.Models
+{
+    [Route("api/medicaments")]
+    [ApiController]
+    public class MedicamentsController : ControllerBase
+    {
+        //private readonly MedicineDbContext _context;
+        //public MedicamentsController(MedicineDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        [HttpGet("{IdMedicament}")]
+        public IActionResult GetMedicament(int idMedicament)
+        {
+            return Ok(idMedicament);
+        }
+
+    }
+}
